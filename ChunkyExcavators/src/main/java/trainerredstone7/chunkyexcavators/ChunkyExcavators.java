427261c2cb2f6bd475fa4d9235c2c123e42b1438
Trainerredstone7 <trainerredstone7@gmail.com>
2020-10-24 16:37:49 -0700

    First commit

---
+++ ChunkyExcavators/src/main/java/trainerredstone7/chunkyexcavators/ChunkyExcavators.java
@@ -0,0 +1,31 @@
+package trainerredstone7.chunkyexcavators;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = ChunkyExcavators.MODID, name = ChunkyExcavators.NAME, version = ChunkyExcavators.VERSION)
+public class ChunkyExcavators
+{
+    public static final String MODID = "chunkyexcavators";
+    public static final String NAME = "Chunky Excavators";
+    public static final String VERSION = "0.1";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
